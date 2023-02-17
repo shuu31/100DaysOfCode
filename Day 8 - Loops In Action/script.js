33314cc600b9f463f1f243335c3b327c213415ac
Jaycob Sadural <102833695+shuu31@users.noreply.github.com>
2023-02-17 10:48:34 -0800

    third section finished

--- Day 8 - Loops In Action/script.js
+++ Day 8 - Loops In Action/script.js
@@ -19,3 +19,42 @@ function calculateSum() {
 }
 
 calculateSumButtonElement.addEventListener('click', calculateSum);
+
+// Second example: highlight links
+
+const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight');
+    }
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Third example: display user data
+
+const dummyUserData = {
+    firstname: 'Shuu',
+    lastname: 'Wang',
+    age: 17
+};
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data');
+
+    outputDataElement.innerHTML = '';
+
+    for (const key in dummyUserData) {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
