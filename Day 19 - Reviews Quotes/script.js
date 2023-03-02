9d8a847c9fb75caa83b865ea0937ed7e5647ed7b
Jaycob Sadural <102833695+shuu31@users.noreply.github.com>
2023-03-01 21:03:55 -0800

    checkpoint

---
+++ Day 19 - Reviews Quotes/script.js
@@ -0,0 +1,14 @@
+const imageList = [
+    "images/susan-smith.jpg",
+    "images/anna-johnson.jpg",
+    "images/peter-jones.jpg",
+    "images/bill-anderson.jpg",
+]
+const nameList = ["Susan Smith", "Anna Johnson", "Peter Jones", "Bill Anderson"];
+const titleList = ["Web Developer", "Web Designer", "Intern", "The Boss"];
+const descriptionList = [
+    "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+]
